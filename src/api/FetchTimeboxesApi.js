3d94446620ebe3efff206acdbd0d3603c72e8fe3
Hubert <huberto777@op.pl>
2020-04-18 13:35:02 +0200

    first commit

---
+++ src/api/FetchTimeboxesApi.js
@@ -0,0 +1,79 @@
+const BASE_URL = "http://localhost:4000/timeboxes";
+const FetchTimeboxesAPI = {
+  getAllTimeboxes: async function() {
+    const response = await makeRequest(BASE_URL, "GET");
+
+    const timeboxes = await response.json();
+    return timeboxes;
+  },
+  addTimebox: async function(timeboxToAdd) {
+    const response = await makeRequest(
+      BASE_URL,
+      "POST",
+
+      timeboxToAdd
+    );
+    const addedTimebox = await response.json();
+    return addedTimebox;
+  },
+  replaceTimebox: async function(timeboxToReplace) {
+    if (!timeboxToReplace.id) {
+      throw new Error("timebox has to have an id to be updated");
+    }
+
+    const response = await makeRequest(
+      `${BASE_URL}/${timeboxToReplace.id}`,
+      "PUT",
+      timeboxToReplace
+    );
+    const replacedTimebox = await response.json();
+    return replacedTimebox;
+  },
+  partiallyUpdateTimebox: async function(timeboxToUpdate) {
+    if (!timeboxToUpdate.id) {
+      throw new Error("timebox has to have an id to be updated");
+    }
+    const response = await makeRequest(
+      `${BASE_URL}/${timeboxToUpdate.id}`,
+      "PATCH",
+      timeboxToUpdate
+    );
+    const updatedTimebox = await response.json();
+    return updatedTimebox;
+  },
+  removeTimebox: async function(timeboxToRemove) {
+    if (!timeboxToRemove.id) {
+      throw new Error("timebox has to have an id to be updated");
+    }
+    await makeRequest(`${BASE_URL}/${timeboxToRemove.id}`, "DELETE");
+  },
+  createTimeboxesAPI: async function(baseUrl) {
+    const response = await makeRequest(baseUrl, "GET");
+
+    const timeboxes = await response.json();
+    return timeboxes;
+  },
+  getTimeboxesByFullTextSearch: async function(searchQuery) {
+    const response = await makeRequest(`${BASE_URL}/?title=${searchQuery}`, "GET");
+    const timeboxes = await response.json();
+    return timeboxes;
+  }
+};
+
+export default FetchTimeboxesAPI;
+
+async function makeRequest(url, method, body) {
+  const jsonBody = body ? JSON.stringify(body) : undefined;
+  const response = await window.fetch(url, {
+    method,
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: jsonBody
+  });
+
+  if (!response.ok) {
+    throw new Error("something went wrong");
+  }
+  return response;
+}
---
+++ src/components/TimeboxList.js
@@ -0,0 +1,100 @@
+import React from "react";
+
+import Timebox from "./Timebox";
+import TimeboxCreator from "./TimeboxCreator";
+import TimeboxesAPI from "../api/FetchTimeboxesApi";
+
+class TimeboxList extends React.Component {
+  state = {
+    timeboxes: [],
+    loading: true,
+    error: null,
+    searchQuery: "",
+  };
+
+  componentDidMount() {
+    // l_7.8/e_2
+    // TimeboxesAPI.createTimeboxesAPI("http://localhost:5000/timeboxes")
+    //   .then(timeboxes => this.setState({ timeboxes }))
+    //   .catch(error => this.setState({ error }))
+    //   .finally(() => this.setState({ loading: false }));
+    TimeboxesAPI.getAllTimeboxes()
+      .then((timeboxes) => this.setState({ timeboxes }))
+      .catch((error) => this.setState({ error }))
+      .finally(() => this.setState({ loading: false }));
+  }
+
+  addTimebox = (timebox) => {
+    TimeboxesAPI.addTimebox(timebox).then((addedTimebox) =>
+      this.setState((prevState) => {
+        const timeboxes = [...prevState.timeboxes, addedTimebox];
+        return { timeboxes };
+      })
+    );
+  };
+  removeTimebox = (indexToRemove) => {
+    TimeboxesAPI.removeTimebox(this.state.timeboxes[indexToRemove]).then(() =>
+      this.setState((prevState) => {
+        const timeboxes = prevState.timeboxes.filter(
+          (timebox, index) => index !== indexToRemove
+        );
+        return { timeboxes };
+      })
+    );
+  };
+  updateTimebox = (id, timeboxToUpdate) => {
+    TimeboxesAPI.partiallyUpdateTimebox(timeboxToUpdate).then(
+      (updatedTimebox) =>
+        this.setState((prevState) => {
+          const timeboxes = prevState.timeboxes.map((timebox) =>
+            timebox.id === id ? updatedTimebox : timebox
+          );
+          return { timeboxes };
+        })
+    );
+  };
+
+  handleCreate = (createdTimebox) => {
+    try {
+      this.addTimebox(createdTimebox);
+    } catch (error) {
+      console.log("Jest błąd przy tworzeniu timeboxa:", error);
+    }
+  };
+  handleSearch = (e) => {
+    this.setState({
+      searchQuery: e.target.value.toLowerCase(),
+    });
+  };
+
+  render() {
+    const timeboxes = TimeboxesAPI.getTimeboxesByFullTextSearch(this.state.searchQuery);
+    return (
+      <>
+        <div className="Timebox">
+          <input placeholder="search" onChange={this.handleSearch} />
+        </div>
+        <TimeboxCreator onCreate={this.handleCreate} />
+        {this.state.loading ? "Timeboxy się ładują..." : null}
+        {this.state.error ? "Nie udało się załadować :(" : null}
+        {timeboxes.map((timebox, index) => (
+          <Timebox
+            key={timebox.id}
+            title={timebox.title}
+            totalTimeInMinutes={timebox.totalTimeInMinutes}
+            onDelete={() => this.removeTimebox(index)}
+            onEdit={() =>
+              this.updateTimebox(timebox.id, {
+                ...timebox,
+                title: "Updated timebox",
+                totalTimeInMinutes: 22,
+              })
+            }
+          />
+        ))}
+      </>
+    );
+  }
+}
+
+export default TimeboxList;
